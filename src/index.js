2afbbc3d649faa6f7bd1965c87250d2b033c6223
darya <korzenok.darya@gmail.com>
2022-05-18 02:27:58 +0300

    Error correction. Checking for correct operation of eslint and prettier.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,40 @@
-//6
-let user = 'John Doe';
-console.log(user);
-let student = 'Darya';
-console.log(student);
-user = student; // user = 'Darya';
-console.log(user);
-
-//7
-let test = 1;
-test += 1;
-test += '1'; // test = '21'
-console.log(test);
-test = Boolean(test); // true
-console.log(test);
-
-//8
 {
-  console.log('Multiplication of elements in array');
+  let user = 'John Doe';
+  console.log(user);
+  const student = 'Darya';
+  console.log(student);
+  user = student; // user = 'Darya';
+  console.log(user);
+}
+{
+  let test = 1;
+  test += 1;
+  test += '1'; // test = '21'
+  console.log(test);
+  test = Boolean(test); // true
+  console.log(test);
+}
+{
   let result = 1;
-  let arr = [2, 3, 5, 8];
-  for (let i = 0; i < arr.length; i++) {
+  const arr = [2, 3, 5, 8];
+  for (let i = 0; i < arr.length; i += 1) {
     result *= arr[i];
   }
-  console.log(result);
+  console.log('Multiplication of elements in array =', result);
 }
-//9
 {
-  console.log('Elements from arrow which >5 and <10');
-  let arr = [2, 5, 8, 15, 0, 6, 20, 3];
-  for (let i = 0; i < arr.length; i++) {
+  const arr = [2, 5, 8, 15, 0, 6, 20, 3];
+  for (let i = 0; i < arr.length; i += 1) {
     if (arr[i] > 5 && arr[i] < 10) {
-      console.log(arr[i]);
+      console.log('Element from arrow which > 5 and < 10:', arr[i]);
     }
   }
 }
-//10
 {
-  console.log('Even elements');
-  let arr = [2, 5, 8, 15, 0, 6, 20, 3];
-  for (let i = 0; i < arr.length; i++) {
+  const arr = [2, 5, 8, 15, 0, 6, 20, 3];
+  for (let i = 0; i < arr.length; i += 1) {
     if (arr[i] % 2 === 0) {
-      console.log(arr[i]);
+      console.log('Even element:', arr[i]);
     }
   }
 }
