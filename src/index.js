4372fa96f71a0faa27bf36a0e4ab08c74bbde9e6
darya <korzenok.darya@gmail.com>
2022-05-20 00:50:14 +0300

    final correction

--- src/index.js
+++ src/index.js
@@ -11,27 +11,24 @@ test += '1'; // test = '21'
 console.log(test);
 test = Boolean(test); // true
 console.log(test);
-{
-  let result = 1;
-  const arr = [2, 3, 5, 8];
-  for (let i = 0; i < arr.length; i += 1) {
-    result *= arr[i];
-  }
-  console.log('Multiplication of elements in array =', result);
+
+let result = 1;
+const arrTask1 = [2, 3, 5, 8];
+for (let i = 0; i < arrTask1.length; i += 1) {
+  result *= arrTask1[i];
 }
-{
-  const arr = [2, 5, 8, 15, 0, 6, 20, 3];
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] > 5 && arr[i] < 10) {
-      console.log('Element from arrow which > 5 and < 10:', arr[i]);
-    }
+console.log('Multiplication of elements in array =', result);
+
+const arrTask2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < arrTask2.length; i += 1) {
+  if (arrTask2[i] > 5 && arrTask2[i] < 10) {
+    console.log('Element from arrow which > 5 and < 10:', arrTask2[i]);
   }
 }
-{
-  const arr = [2, 5, 8, 15, 0, 6, 20, 3];
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] % 2 === 0) {
-      console.log('Even element:', arr[i]);
-    }
+
+const arrTask3 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < arrTask3.length; i += 1) {
+  if (arrTask3[i] % 2 === 0) {
+    console.log('Even element:', arrTask3[i]);
   }
 }
